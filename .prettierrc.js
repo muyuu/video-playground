4cd247e0e3f9161d6ee31d3da710ca9635adbc26
muyuu <anticyborg@gmail.com>
2021-03-04 17:40:01 +0900

    add setting files

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+// See https://github.com/abema/abema-web/issues/4921
+module.exports = {
+    printWidth: 100,
+    tabWidth: 4,
+    useTabs: false,
+    singleQuote: true,
+    trailingComma: 'all',
+    arrowParens: 'always',
+};
